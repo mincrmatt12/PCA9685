921a32b9a76e940c822ca9fc1d0613262305c3bb
mincrmatt12 <matthewmirvish@hotmail.com>
2017-07-26 21:28:58 -0400

    Initial commit

---
+++ src/PCA9685.h
@@ -0,0 +1,27 @@
+#ifndef PCA9685_LIBRARY_H
+#define PCA9685_LIBRARY_H
+
+#include <cstdint>
+#include <cstdio>
+
+const uint16_t PRESCALE = 11; //hz
+
+class PCA9685 {
+public:
+    PCA9685(int address, uint8_t bus);
+
+    void init();
+
+    void setPWM(int channel, int on, int off);
+
+private:
+    int i2cHandle;
+
+    void setRegister(uint8_t reg, uint8_t val);
+    int getRegister(uint8_t reg);
+
+    int address;
+    uint8_t bus;
+};
+
+#endif
---
+++ test/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by matthew on 26/07/17.
+//
+
+#include <PCA9685.h>
+
+PCA9685 pca9685(0x40, 0);
+
+void setServo(int channel, int pulse) {
+    int pulse_length = 1000000 / 500 / 4096;
+    int pulse2 = pulse * 1000;
+    pulse2 /= pulse_length;
+    pca9685.setPWM(channel, 0, pulse);
+}
+
+void main(int argc, char* argv[]) {
+    pca9685.init();
+
+    setServo(0, 2200);
+    setServo(1, 2200);
+}
